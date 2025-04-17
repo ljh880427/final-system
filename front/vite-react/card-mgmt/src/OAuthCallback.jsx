0e76b11ef65734e5fbf0e921dc006d535854ce8d
ljh <ljh5432@gmail.com>
2025-04-17 18:46:25 +0900

    modify

--- front/vite-react/card-mgmt/src/OAuthCallback.jsx
+++ front/vite-react/card-mgmt/src/OAuthCallback.jsx
@@ -7,9 +7,8 @@ export default function OAuthCallback() {
   const navigate = useNavigate();
   const [code, setCode] = useState(null);
 
-  const isCalled = useRef(false); // 중복요청 방지1 
+  const isCalled = useRef(false); // 중복요청 방지1
 
-  // ✅ OAuth code 처리 로직
   useEffect(() => {
     if (isCalled.current) return;
     isCalled.current = true;
@@ -23,11 +22,7 @@ export default function OAuthCallback() {
       setCode(codeParam); // code 값 셋팅
 
       try {
-        const response = await axios.post(
-          `/api/TokenFromAuthCode`,
-          { code: codeParam },
-          { withCredentials: true }
-        );
+        const response = await axios.post(`/api/TokenFromAuthCode`, { code: codeParam }, { withCredentials: true });
         if (response.data.status === true) {
           navigate('/');
         } else {
