79c62829f24cc07b8b6935ee63d11fded5af54a9
ljh <ljh5432@gmail.com>
2025-04-19 16:32:47 +0900

    test

--- backend/spring-boot/oauth/oauth2/oauth2/src/main/java/com/java/oauth2/common/Utils.java
+++ backend/spring-boot/oauth/oauth2/oauth2/src/main/java/com/java/oauth2/common/Utils.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
