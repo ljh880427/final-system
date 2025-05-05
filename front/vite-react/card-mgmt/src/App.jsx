a82af637cd7b663f73bd9d528d86fa2613fdab8e
ljh <ljh5432@gmail.com>
2025-05-05 15:49:50 +0900

    modify

--- front/vite-react/card-mgmt/src/App.jsx
+++ front/vite-react/card-mgmt/src/App.jsx
@@ -15,14 +15,14 @@ import PrivateRoute from './PrivateRoute'; // 새로 추가
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />} />
+      <Routes>        
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/callback/custom" element={<OAuthCallback />} />
         
         {/* 보호된 라우트 */}
+        <Route path="/" element={<PrivateRoute><MainLayout /></PrivateRoute>} />
         <Route path="/detail/:id" element={<PrivateRoute><CardDetail /></PrivateRoute>} />
         <Route path="/edit/:cardNo" element={<PrivateRoute><CardEdit /></PrivateRoute>} />
         <Route path="/MyPageInfo/:id" element={<PrivateRoute><MyPageInfo /></PrivateRoute>} />
